c8ef44eb9d239eb0f02f3efe79994a2e0632cf94
Yizhe ZHANG <elecoxy@gmail.com>
2023-05-02 09:59:44 +0800

    1491. Average Salary Excluding the Minimum and Maximum Salary

--- 1491. Average Salary Excluding the Minimum and Maximum Salary/1491. Average Salary Excluding the Minimum and Maximum Salary.py
+++ 1491. Average Salary Excluding the Minimum and Maximum Salary/1491. Average Salary Excluding the Minimum and Maximum Salary.py
@@ -1,13 +1,19 @@
 from typing import List
 
 class Solution:
+    # def average(self, salary: List[int]) -> float:
+    #
+    #     salary.sort()
+    #     salary[:] = salary[1:-1]
+    #
+    #
+    #     return sum(salary) / len(salary)
+
     def average(self, salary: List[int]) -> float:
-
         salary.sort()
-        salary[:] = salary[1:-1]
+        return sum(salary[1:-1]) / (len(salary) - 2)
 
 
-        return sum(salary) / len(salary)
 
 salary = [4000,3000,1000,2000]
 # salary = [1000,2000,3000]
